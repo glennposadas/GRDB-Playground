4fb2679a0a434cd848e4fd7e820d73e5305c955f
Glenn Posadas <hello@glennvon.com>
2022-05-26 02:24:14 +0800

    add working add post + diffable source.

--- GRDB-Playground/ViewController.swift
+++ GRDB-Playground/ViewController.swift
@@ -9,8 +9,9 @@ import Combine
 import GRDB
 import UIKit
 
+// MARK: - ViewModel
+
 class VM {
-  var cancellables = Set<AnyCancellable>()
   
   var postPublisher: DatabasePublishers.Value<[Post]> {
     let dbWriter = AppDB.shared.dbWriter
@@ -23,18 +24,22 @@ class VM {
     return publisher
   }
   
-  func start() {
-    postPublisher
-      .removeDuplicates()
-      .sink { completion in
-        
-      } receiveValue: { posts in
-                
-        print("===============")
-        print("RECEIVE VALUE! ✅: \(posts)")
-        print("===============")
-        
-      }.store(in: &cancellables)
+  func newPost() -> Post {
+    Post(id: UUID().uuidString, content: Lorem.shortTweet, createdAt: Date())
+  }
+  
+  func savePost(_ post: Post) {
+    do {
+      try AppDB.shared.dbWriter.write { db in
+        var p = post
+        try p.save(db)
+      }
+      
+      print("Saved!")
+      
+    } catch {
+      print("ERROR!: \(error.localizedDescription)")
+    }
   }
   
   func post(_ id: String) -> Post? {
@@ -51,14 +56,68 @@ class VM {
   }
 }
 
-class ViewController: UIViewController {
+// MARK: - ViewController
 
+
+class ViewController: UITableViewController {
+  
+  var cancellables = Set<AnyCancellable>()
+  
   let vm = VM()
+  
+  typealias DS = UITableViewDiffableDataSource<Int, Post>
+  
+  lazy var datasource: DS = {
+    let datasource = DS(tableView: tableView, cellProvider: { (tableView, indexPath, post) -> Cell? in
+      let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as? Cell
+      cell?.idLabel.text = "Idx: \(post.idx ?? 0)\nId: \(post.id)"
+      cell?.contentLabel.text = post.content
+      return cell
+    })
     
+    return datasource
+  }()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    
-    vm.start()
+    vm
+      .postPublisher
+      .removeDuplicates()
+      .sink { completion in
+        
+      } receiveValue: { [weak self] posts in
+        
+        guard let self = self else {
+          return
+        }
+        
+        print("===============")
+        print("RECEIVE VALUE! ✅: \(posts)")
+        print("===============")
+        
+        var snapshot = self.datasource.snapshot()
+        snapshot.deleteAllItems()
+        
+        snapshot.appendSections([0])
+        snapshot.appendItems(posts, toSection: 0)
+        self.datasource.apply(snapshot)
+        
+        self.tableView.scrollToRow(at: IndexPath(row: posts.count - 1, section: 0), at: .bottom, animated: true)
+        
+      }.store(in: &cancellables)
   }
+  
+  @IBAction func newPost(_ sender: Any) {
+    let p = vm.newPost()
+    vm.savePost(p)
+  }
+  
+}
+
+// MARK: - UITableViewCell
+
+class Cell: UITableViewCell {
+  @IBOutlet weak var idLabel: UILabel!
+  @IBOutlet weak var contentLabel: UILabel!
 }
